f04590f2f01f588cc2db4c0e257a900051790c80
EvaSzi <moze80@gmail.com>
2026-05-12 16:48:17 +0100

    2026051`

---
+++ VGY_gyak1.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+adatok=(12 9 15 11 7 13 16)
+
+darab=${#adatok[@]}
+
+echo "Ennyi napi hőmérsékleti adat van: $darab"
+
+osszeg=0
+
+for adat in "${adatok[@]}"
+do
+    osszeg=$((osszeg + adat))
+done
+
+echo "A hőmérsékletek összege: $osszeg."
+
+atlag=$((osszeg / darab))
+
+echo "Átlag: $atlag."
+
+minimum=${adatok[0]}
+
+for i in "${adatok[@]}"
+do
+    if [ "$i" -lt "$minimum" ]; then
+        minimum=$i
+    fi
+done
+
+echo "A legalacsonyabb hőmérséklet: $minimum"
+
+maximum=${adatok[0]}
+
+for i in "${adatok[@]}"
+do
+    if [ "$i" -gt "$maximum" ];
+    then maximum=$i
+    fi
+done
+
+echo "A legmagasabb hőmérséklet: $maximum"
+
+hetfok=0
+
+for i in "${adatok[@]}"
+do
+    if [ "$i" -eq 7 ]
+    then
+    hetfok=$((hetfok + 1))
+    fi
+done
+
+echo "A 7 fokos érték $hetfok-szor szerepel."
